34ae2af54ab3e0e0904130bbf4961dda85b75247
aminhp93 <minhpn.org.ec@gmail.com>
2016-10-28 14:49:02 -0700

    swift fundamental

---
+++ testing.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var type:String = "Rectangle"
+var description:String = "A quadrilateral with four right angles"
+var width:Double = 5
+var height:Double = 10.5
+var area:Double = width * height
+height += 1
+width += 1
+area = width * height
+print("The shape is a \(type) or \(description) with an area of \(area)")
+
+var numberOfChampionships = 5
+let name = "Kobe"
+print("My favrite player is \(name) and he has \(numberOfChampionships) rings")
+print("His jersay number is \(8*3)")
